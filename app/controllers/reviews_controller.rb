65ee24ca81ae50295560f063b9f7db7d32c5a9b4
Garland Lai <garlandlai24@gmail.com>
2019-08-23 09:51:08 -0700

    add reviews controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,36 @@
 class ReviewsController < ApplicationController
 
+  def index
+    @restaurants = Restaurant.find(params[:restaurant_id])
+    @reviews = @restaurants.reviews.all
+    json_response(@reviews)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.find(params[:id])
+    json_response(@review)
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.create!(reviews_params)
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.find(params[:id])
+    @reviews.update!(review_params)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reviews = @restaurant.reviews.find(params[:id])
+    @reviews.update!(reviews_params)
+  end
+
+  private
+  def review_params
+    params.permit(:name, :rating, :content)
+  end
 end
